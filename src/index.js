d9596d9b4792fa305faa54ce676945c13f879109
FTRHOST <fathironmy4@gmail.com>
2026-06-25 23:13:54 +0700

    feat: add battle chat command implementation

--- src/index.js
+++ src/index.js
@@ -285,8 +285,54 @@ function executeSimpleHooks() {
   const UIRankHero = Assembly.class("UIRankHero");
   const GameInit = Assembly.class("GameInit");
   const ActLclCfgMgr = Assembly.class("ActLclCfgMgr");
+  const BattleBridge = Assembly.class("BattleBridge");
 
   showGameNotification("MLLEAK", "v.0.1");
+
+  const ShowChatHistoryText = BattleBridge.method("ShowChatHistoryText");
+
+  // Mengganti (intercept) implementasi fungsi aslinya
+  ShowChatHistoryText.implementation = function (messageObj) {
+    // Casting agar TypeScript mengenali objek ini sebagai String Il2Cpp
+    const il2cppStr = messageObj;
+    const rawContent = il2cppStr.content;
+
+    if (rawContent) {
+      const msg = rawContent.toString();
+
+      // 1. Gunakan matchAll dengan flag /g (global) untuk mencari SEMUA kecocokan di seluruh riwayat chat
+      // Spread operator [...] akan mengubah hasilnya menjadi sebuah Array
+      const matches = [...msg.matchAll(/#(\w+)/g)];
+
+      // 2. Cek apakah ada setidaknya satu perintah yang ditemukan
+      if (matches.length > 0) {
+        // 3. Ambil elemen paling terakhir dari array (yaitu chat paling bawah/terbaru)
+        const lastMatch = matches[matches.length - 1];
+
+        // lastMatch[1] berisi kata sandinya (capture group pertama)
+        if (lastMatch && lastMatch[1]) {
+          const cmd = lastMatch[1].toLowerCase();
+          console.log(`[Command] Detected: ${cmd}`);
+
+          // ... logika eksekusi perintah (hideui, dll) ...
+          if (cmd == "hideui") {
+            Il2Cpp.gc.choose(BattleBridge).forEach((instance) => {
+              console.log(
+                `[*] Found UIController instance at ${instance.handle}`,
+              );
+
+              // Melakukan invoke method pada instance tersebut
+              // Ekuivalen dengan il2cpp_runtime_invoke
+              instance.method("ToggleAllUIShow").invoke();
+            });
+          }
+        }
+      }
+    }
+    // Perbaikan 1: Gunakan '!' pada method("OnSend")
+    // Perbaikan 2: Gunakan il2cppStr.handle alih-alih messageObj.handle
+    return this.method("ShowChatHistoryText").invoke(il2cppStr.handle);
+  };
 
   // Hook 1: Free Skin Mod (Restricted to VIP and Admin)
   const BActFreeSkin = ChooseHeroMgr.method("BActFreeSkin");
