f525c3c566166a1a0fe26a60c876333408b7b769
FTRHOST <fathironmy4@gmail.com>
2026-07-05 20:36:13 +0700

    fix(battle_commands): fix Il2Cpp method invocation target reference (this instead of gc.choose)

--- src/mods/battle_commands.js
+++ src/mods/battle_commands.js
@@ -10,12 +10,10 @@ export function setupBattleCommands(Assembly) {
 
   const ShowChatHistoryText = BattleBridge.method("ShowChatHistoryText");
   if (ShowChatHistoryText) {
-    ShowChatHistoryText.implementation = function (messageObj) {
-      const il2cppStr = messageObj;
-      const rawContent = il2cppStr.content;
-
-      if (rawContent) {
-        const msg = rawContent.toString();
+    ShowChatHistoryText.implementation = function (messageStr) {
+      if (messageStr) {
+        // Karena parameternya System.String, kita baca string-nya langsung
+        const msg = messageStr.content ? messageStr.content.toString() : messageStr.toString();
         const matches = [...msg.matchAll(/#(\w+)/g)];
 
         if (matches.length > 0) {
@@ -31,36 +29,35 @@ export function setupBattleCommands(Assembly) {
                 "[00FF00]#help[-]: For show all command\n[00FF00]#hideui[-]: For hide all ui on battle\n[00FF00]#hidebar[-]: For hide bar health on battle\n[00ff00]#hidename[-]: For hide name only",
               );
             } else if (cmd == "hideui" || cmd == "hidebar" || cmd == "hidename") {
-              // Lazy-load instance just when needed for performance
-              const instanceBattleBridge = Il2Cpp.gc.choose(BattleBridge);
-              if (instanceBattleBridge.length > 0) {
-                const objekAktifBattleBridge = instanceBattleBridge[0];
-                try {
-                  if (cmd == "hideui") {
-                    // Specify 0 parameters in case of method overloading / obfuscation confusion
-                    const mToggle = objekAktifBattleBridge.method("ToggleAllUIShow", 0) || objekAktifBattleBridge.method("ToggleAllUIShow");
-                    if(mToggle) mToggle.invoke();
-                    else console.log("[-] Method ToggleAllUIShow not found");
-                  } else if (cmd == "hidebar") {
-                    const mHideBar = objekAktifBattleBridge.method("SetHeroBloodShow", 1) || objekAktifBattleBridge.method("SetHeroBloodShow");
-                    if(mHideBar) mHideBar.invoke(false);
-                    else console.log("[-] Method SetHeroBloodShow not found");
-                  } else if (cmd == "hidename") {
-                    const mHideName = objekAktifBattleBridge.method("HideHeroNameAndFly", 1) || objekAktifBattleBridge.method("HideHeroNameAndFly");
-                    if(mHideName) mHideName.invoke(true);
-                    else console.log("[-] Method HideHeroNameAndFly not found");
-                  }
-                } catch (e) {
-                  console.log(`[-] Error invoking command ${cmd}: ${e.message}`);
+              // Harus menggunakan 'this' (instance pemanggil asli dari BattleBridge)
+              // daripada mengandalkan gc.choose yang tidak dapat diandalkan
+              const activeBattleBridge = this;
+              
+              try {
+                if (cmd == "hideui") {
+                  const mToggle = activeBattleBridge.method("ToggleAllUIShow", 0) || activeBattleBridge.method("ToggleAllUIShow");
+                  if(mToggle) mToggle.invoke();
+                  else console.log("[-] Method ToggleAllUIShow not found");
+                } else if (cmd == "hidebar") {
+                  const mHideBar = activeBattleBridge.method("SetHeroBloodShow", 1) || activeBattleBridge.method("SetHeroBloodShow");
+                  if(mHideBar) mHideBar.invoke(false);
+                  else console.log("[-] Method SetHeroBloodShow not found");
+                } else if (cmd == "hidename") {
+                  const mHideName = activeBattleBridge.method("HideHeroNameAndFly", 1) || activeBattleBridge.method("HideHeroNameAndFly");
+                  if(mHideName) mHideName.invoke(true);
+                  else console.log("[-] Method HideHeroNameAndFly not found");
                 }
-              } else {
-                 console.log("[-] No BattleBridge instance found");
+              } catch (e) {
+                console.log(`[-] Error invoking command ${cmd}: ${e.message}`);
               }
             }
           }
         }
       }
-      return ShowChatHistoryText.invoke(this, messageObj);
+      
+      // Untuk instance method, kita harus memanggil fungsi aslinya seperti ini di frida-il2cpp-bridge:
+      // Menggunakan 'this' sebagai instance context, bukan 'ShowChatHistoryText.invoke()'
+      return ShowChatHistoryText.invoke(this, messageStr);
     };
   }
 }
