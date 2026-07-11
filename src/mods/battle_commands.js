3e53e595284f000fa2fd95a29214f5824b47ad70
FTRHOST <fathironmy4@gmail.com>
2026-07-11 15:02:23 +0700

    feat: new feature for no cd

--- src/mods/battle_commands.js
+++ src/mods/battle_commands.js
@@ -6,6 +6,10 @@ import { showGameNotification } from "../index";
 
 export function setupBattleCommands(Assembly) {
   const BattleBridge = Assembly.class("BattleBridge");
+  const CoolDownData = Assembly.class("CoolDownData");
+
+  const EnterCoolDown = CoolDownData.method("EnterCoolDown");
+
   if (!BattleBridge || BattleBridge.handle.isNull()) return;
 
   const ShowChatHistoryText = BattleBridge.method("ShowChatHistoryText");
@@ -13,7 +17,9 @@ export function setupBattleCommands(Assembly) {
     ShowChatHistoryText.implementation = function (messageStr) {
       if (messageStr) {
         // Karena parameternya System.String, kita baca string-nya langsung
-        const msg = messageStr.content ? messageStr.content.toString() : messageStr.toString();
+        const msg = messageStr.content
+          ? messageStr.content.toString()
+          : messageStr.toString();
         const matches = [...msg.matchAll(/#(\w+)/g)];
 
         if (matches.length > 0) {
@@ -26,29 +32,23 @@ export function setupBattleCommands(Assembly) {
             if (cmd == "help") {
               showGameNotification(
                 "Battle Command",
-                "[00FF00]#help[-]: For show all command\nSorry to say method for hide ui has been change by moonton",
+                "[00FF00]#help[-]: For show all command\n[00FF00]#nocd[-]: For no cooldown\nSorry to say method for hide ui has been change by moonton",
               );
-            } /* else if (cmd == "hideui" || cmd == "hidebar" || cmd == "hidename") {
-              const activeBattleBridge = this;
-              try {
-                if (cmd == "hideui") {
-                  const mToggle = activeBattleBridge.method("ToggleAllUIShow", 0) || activeBattleBridge.method("ToggleAllUIShow");
-                  if(mToggle) mToggle.invoke();
-                } else if (cmd == "hidebar") {
-                  const mHideBar = activeBattleBridge.method("SetHeroBloodShow", 1) || activeBattleBridge.method("SetHeroBloodShow");
-                  if(mHideBar) mHideBar.invoke(false);
-                } else if (cmd == "hidename") {
-                  const mHideName = activeBattleBridge.method("HideHeroNameAndFly", 1) || activeBattleBridge.method("HideHeroNameAndFly");
-                  if(mHideName) mHideName.invoke(true);
-                }
-              } catch (e) {
-                console.log(`[-] Error invoking command ${cmd}: ${e.message}`);
-              }
-            } */
+            } else if (cmd == "nocd") {
+              Interceptor.attach(EnterCoolDown, {
+                onEnter: function (args) {
+                  // Blok ini kosong, fungsi asli tidak akan mengeksekusi
+                  // instruksi apapun sampai keluar dari fungsi.
+                },
+                onLeave: function (retval) {
+                  // Opsional: Anda juga bisa mengubah nilai balik (*return value*)
+                },
+              });
+            }
           }
         }
       }
-      
+
       // Untuk instance method, kita harus memanggil fungsi aslinya seperti ini di frida-il2cpp-bridge:
       // Menggunakan 'this' sebagai instance context, bukan 'ShowChatHistoryText.invoke()'
       return ShowChatHistoryText.invoke(this, messageStr);
