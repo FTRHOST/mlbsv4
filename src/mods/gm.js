4e8a9509cd9c07e112a32c52ffcad8b910f0c232
FTRHOST <fathironmy4@gmail.com>
2026-08-31 16:16:46 +0700

    disable hook gm

--- src/mods/gm.js
+++ src/mods/gm.js
@@ -33,7 +33,7 @@ export function setupGMHooks(Assembly) {
   hookSandboxMethod("GameInit", "IsSandBoxIp");
   // hookSandboxMethod("LuaHelper", "IsEditor");
   // hookSandboxMethod("LuaHelper", "IsTestChannel");
-  hookSandboxMethod("SDKCommon", "IsSandbox");
+  // hookSandboxMethod("SDKCommon", "IsSandbox");
   //hookSandboxMethod("PingServerData", "CheckInTestServer");
   // hookSandboxMethod("PingServerData", "CheckIsTestServer");
   // hookSandboxMethod("GMVideoPlayer", "IsGMBackend");
@@ -94,98 +94,9 @@ export function setupGMHooks(Assembly) {
   // --- EXTENDED GM UI & PROFILER HOOKS ---
 
   if (sessionState.isAuthorized && sessionState.permissions.allowGMMode) {
-    // 1. Hook GameServerConfig (System Gatekeeper)
-    const GameServerConfig = Assembly.class("GameServerConfig");
-    if (GameServerConfig && !GameServerConfig.handle.isNull()) {
-      // Set static fields via .cctor or wait for instance
-      const cctor = GameServerConfig.method(".cctor");
-      if (cctor) {
-        Interceptor.attach(cctor.virtualAddress, {
-          onLeave: function () {
-            try {
-              const instance = GameServerConfig.field("Instance").value;
-              if (instance && !instance.isNull()) {
-                instance.field("m_bGSDKSandBox").value = true;
-                instance.field("m_bAdjustSandBox").value = true;
-                debugLog(
-                  "GM Mod",
-                  "GameServerConfig Instance fields set to true.",
-                );
-              }
-            } catch (e) {}
-          },
-        });
-      }
-
-      // Fallback: Try setting instance directly if already exists
-      try {
-        const instance = GameServerConfig.field("Instance").value;
-        if (instance && !instance.isNull()) {
-          instance.field("m_bGSDKSandBox").value = true;
-          instance.field("m_bAdjustSandBox").value = true;
-          debugLog(
-            "GM Mod",
-            "GameServerConfig Instance (direct) fields set to true.",
-          );
-        }
-      } catch (e) {}
-    }
-
-    // 2. Hook LoginCLibraryUtils (Native Sandbox Flag)
-    const LoginCLibraryUtils = Assembly.class("LoginCLibraryUtils");
-    if (LoginCLibraryUtils && !LoginCLibraryUtils.handle.isNull()) {
-      try {
-        const field = LoginCLibraryUtils.field("mStaticIsSandBox");
-        if (field) {
-          field.value = true;
-          debugLog(
-            "GM Mod",
-            "LoginCLibraryUtils.mStaticIsSandBox set to true.",
-          );
-        }
-      } catch (e) {}
-    }
-
-    // 3. Hook AdrenoStatistics (Profiler UI)
-    const AdrenoStats = Assembly.class("AdrenoStatistics");
-    if (AdrenoStats && !AdrenoStats.handle.isNull()) {
-      try {
-        const instance = AdrenoStats.field("_instance").value;
-        if (instance && !instance.isNull()) {
-          instance.field("_isQprofilerInited").value = true;
-          instance.field("isInit").value = true;
-          debugLog("GM Mod", "AdrenoStatistics (Profiler) enabled.");
-        }
-      } catch (e) {}
-
-      // Hook .ctor to catch it early
-      const ctor = AdrenoStats.method(".ctor");
-      if (ctor) {
-        Interceptor.attach(ctor.virtualAddress, {
-          onLeave: function (retval) {
-            const instance = this.handle;
-            if (instance && !instance.isNull()) {
-              instance.field("_isQprofilerInited").value = true;
-              instance.field("isInit").value = true;
-              debugLog(
-                "GM Mod",
-                "AdrenoStatistics (Profiler) enabled in .ctor.",
-              );
-            }
-          },
-        });
-      }
-    }
-
     // 4. Hook GameInit for Login GM UI
     const GameInit = Assembly.class("GameInit");
     if (GameInit && !GameInit.handle.isNull()) {
-      // Set some static flags that might help
-      try {
-        const opLog = GameInit.field("IsOpenEditorLoadLog");
-        if (opLog) opLog.value = true;
-      } catch (e) {}
-
       // Hook constructor to set instance field _bGmLogin
       const ctor = GameInit.method(".ctor");
       if (ctor) {
